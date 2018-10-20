d023eb434bf1a59a9f44f2c4b5bb3d18d9217a75
DavidWells <hello@davidwells.io>
2018-10-20 16:21:39 -0700

    init commit

---
+++ packages/testing-lerna-one/index.test.js
@@ -0,0 +1,7 @@
+import test from 'ava';
+const one = require('./index')
+
+
+test('arrays are equal', t => {
+	t.is(one, 'beta')
+});
---
+++ packages/testing-lerna-usage/index.js
@@ -0,0 +1,3 @@
+var one = require('testing-lerna-one')
+var two = require('testing-lerna-two')
+console.log(one + " " + two)
